01c9de0e93a46b1907f4d35dda83978771fbb081
gawaikalpana19 <kalpanagajanangawai1996@gmail.com>
2022-11-16 19:40:35 +0530

    First commit

---
+++ Demo_Project/src/com/velocity/collection/demo/LinkedlistDemo.java
@@ -0,0 +1,19 @@
+package com.velocity.collection.demo;
+
+import java.util.LinkedList;
+
+public class LinkedlistDemo {
+
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.add(10);
+		linkedList.add(20);
+		linkedList.add(30);
+		linkedList.add(40);
+		linkedList.add(50);
+	System.out.println("linkedlist having contains:" +linkedList);	
+		
+		
+	}
+
+}
